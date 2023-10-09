753b6289ed57bfd94c107fa45cca544e90cacd31
Michelle Weirathmueller <mweirathmueller@jasco.com>
2023-10-09 11:04:09 -0400

    add a schemas python file.

--- try_import_data.py
+++ try_import_data.py
@@ -3,14 +3,13 @@ Import CGM and Loop (insulin pump) data from mongo atlas.
 """
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
-from datetime import datetime
 import yaml
 import mdb_tools
+
 # Pyarrow - importing mongo databases into pandas
-import pyarrow
-import bson
 import pymongoarrow.monkey
-from pymongoarrow.api import Schema, find_pandas_all
+from schemas import mdb_schemas
+
 # Add extra find_* methods to pymongo collection objects (pymongoarrow):
 pymongoarrow.monkey.patch_all()
 
@@ -38,23 +37,8 @@ col_entries = db["entries"]
 # load the "treatments" collection - this is the one that includes loop recordings of boluses, basal, etc
 col_treatments = db["treatments"]
 
-# Make the schemas
-entries_schema = Schema({
-    'sgv': float,
-    'dateString': str,
-})
 
-treatment_schema = Schema({
-    'duration': float,
-    'amount': float,
-    'absolute': float,
-    'timestamp': str,
-    'created_at': str,
-    'rate': float,
-    'temp': str,
-    'automatic': bool,
-    'eventType': str,
-})
+treatment_schema, entries_schema = mdb_schemas()
 
 df_entries = col_entries.find_pandas_all({}, schema=entries_schema)
 df_treatment = col_treatments.find_pandas_all({}, schema=treatment_schema)
